e6ec0699ab1fe4b71ad41c872d1ea476ff14b915
chrismeim <144551793+chrismeim@users.noreply.github.com>
2023-10-17 00:33:18 +0100

    Update Excercise1.R

--- Excercise1.R
+++ Excercise1.R
@@ -3,7 +3,7 @@ Introduction
 The aim of this excercise is to create a multiple linear regression model that will explain the "excess returns" of a stock,
 taking into account the 3 factor fama-french model (Market Risk, Size Premium and Value Premium) 
 
-In the first part of the excercise I calculate daily returns for up to six stocks in the data set,
+In the first part of the excercise I calculate daily returns for up to 5 stocks in the data set,
 and based on a simple metric, I will choose one stock for further analysis.
 
 In the second part of the excercise I load and prepare prepare the `fama_french_daily.csv` data,
